44db96ff481c7f5c33b30d383d52617991adcd47
Dhruvishdp <dhruvishpatel22@gmail.com>
2021-04-27 15:59:14 +0530

    splasscreen complete 27 april

--- app/src/main/java/com/digitalreceipt/app/SplashScreen.kt
+++ app/src/main/java/com/digitalreceipt/app/SplashScreen.kt
@@ -1,19 +1,31 @@
 package com.digitalreceipt.app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class SplashScreen : AppCompatActivity() {
     lateinit var imageView: ImageView
+    lateinit var TextView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
         imageView=findViewById(R.id.imagelogo)
-        var animat=AnimationUtils.loadAnimation(applicationContext,R.anim.alphafade)
-        imageView.animation=animat
+        var animatfade=AnimationUtils.loadAnimation(applicationContext,R.anim.alphafade)
+        imageView.animation=animatfade
+        TextView=findViewById(R.id.splashtext)
+        var animatmove=AnimationUtils.loadAnimation(applicationContext,R.anim.movement)
+        TextView.animation=animatmove
+        Handler().postDelayed({
+            var intent=Intent(applicationContext,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
     }
 }
