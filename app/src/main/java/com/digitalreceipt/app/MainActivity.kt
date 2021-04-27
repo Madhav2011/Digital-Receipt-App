0a1d76869260bb83044d9484ea95a863bc51da45
Madhav4532 <madhavgabani@gmail.com>
2021-04-27 21:59:27 +0530

    lgin admin

--- app/src/main/java/com/digitalreceipt/app/MainActivity.kt
+++ app/src/main/java/com/digitalreceipt/app/MainActivity.kt
@@ -3,9 +3,12 @@ package com.digitalreceipt.app
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import androidx.appcompat.widget.AppCompatButton
 
 class MainActivity : AppCompatActivity() {
+    lateinit var entUname:EditText
+    lateinit var entUpass:EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,7 +20,14 @@ class MainActivity : AppCompatActivity() {
             var intent: Intent = Intent(applicationContext, Registerpage::class.java)
             startActivity(intent)
         }
-        buttonlogin.setOnClickListener {
+        /*buttonlogin.setOnClickListener {
+            var intent: Intent = Intent(applicationContext, homepage1::class.java)
+            startActivity(intent)
+        }*/
+        entUname=findViewById(R.id.entusername)
+        entUpass=findViewById(R.id.entpassword)
+        if (entUname.equals("admin") && entUpass.equals("admin"))
+        {
             var intent: Intent = Intent(applicationContext, homepage1::class.java)
             startActivity(intent)
         }
