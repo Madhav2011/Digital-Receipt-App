b1d528e205f7346182b4d6ce8d64a4ecb059beed
Madhav2011 <madhavgabani@gmail.com>
2021-04-29 12:27:33 +0530

    Login Admin admin Credentials

--- app/src/main/java/com/digitalreceipt/app/MainActivity.kt
+++ app/src/main/java/com/digitalreceipt/app/MainActivity.kt
@@ -8,40 +8,43 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
 import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
-    /*lateinit var entUname:TextInputEditText
-    lateinit var entUpass:TextInputEditText
-    lateinit var btnLogin:AppCompatButton*/
+    lateinit var entUname:TextInputLayout
+    lateinit var entUpass:TextInputLayout
+    lateinit var btnLogin:AppCompatButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var buttonlogin: AppCompatButton = findViewById(R.id.btlogin)
+        /*var buttonlogin: AppCompatButton = findViewById(R.id.btlogin)*/
         var buttonregister: AppCompatButton = findViewById(R.id.btregister)
 
         buttonregister.setOnClickListener {
             var intent: Intent = Intent(applicationContext, Registerpage::class.java)
             startActivity(intent)
         }
-        buttonlogin.setOnClickListener {
+        /*buttonlogin.setOnClickListener {
             var intent: Intent = Intent(applicationContext, homepage1::class.java)
             startActivity(intent)
-        }
-        /*entUname=findViewById(R.id.entusername)
+        }*/
+        entUname=findViewById(R.id.entusername)
         entUpass=findViewById(R.id.entpassword)
         btnLogin=findViewById(R.id.btlogin)
         btnLogin.setOnClickListener {
-            var uname=entUname.text.toString()
-            var pass=entUpass.text.toString()
+            var uname=entUname.editText!!.text.toString()
+            var pass= entUpass.editText!!.text.toString()
             if(uname.equals("admin") && pass.equals("admin"))
             {
-                Toast.makeText(applicationContext,"Login Success!!",Toast.LENGTH_LONG).show()
+                Toast.makeText(applicationContext,"Login Successful!!",Toast.LENGTH_LONG).show()
+                    var intent: Intent = Intent(applicationContext, homepage1::class.java)
+                    startActivity(intent)
             }
             else
             {
                 Toast.makeText(applicationContext,"Invalid Credential!!",Toast.LENGTH_LONG).show()
             }
-        }*/
+        }
     }
 }
